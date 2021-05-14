16199fb6f6626fd7972d90ad077b2023167fd21e
TariqBazadough <tariq.bazadough@gmail.com>
2021-05-14 17:45:57 +0300

    get all articles function

--- main.js
+++ main.js
@@ -3,6 +3,36 @@ const app = express();
 
 const port = 5000;
 
-app.listen(() => {
+app.use(express.json());
+
+const articles = [
+  {
+    id: 1,
+    title: "How I learn coding?",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+  {
+    id: 2,
+    title: "Coding Best Practices",
+    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+    author: "Besslan",
+  },
+  {
+    id: 3,
+    title: "Debugging",
+    description: "Lorem, Quam, mollitia.",
+    author: "Jouza",
+  },
+];
+
+const getAllArticles = (req, res) => {
+  res.status(200);
+  res.json(articles);
+};
+
+app.get("/articles", getAllArticles);
+
+app.listen(port, () => {
   console.log(`Server is working on Port : ${port}`);
 });
