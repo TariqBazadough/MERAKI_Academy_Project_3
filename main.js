6a75d00d0206c78ec0f0cc7fbcc3048126d4ba5a
TariqBazadough <tariq.bazadough@gmail.com>
2021-05-15 17:46:11 +0300

    done with function updateAnArticleById

--- main.js
+++ main.js
@@ -68,10 +68,34 @@ const createNewArticle = (req, res) => {
   res.json(newArticle);
 };
 
+const updateAnArticleById = (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const author = req.body.author;
+  let i;
+  const found = articles.find((element, index) => {
+    i = index;
+    return element.id == id;
+  });
+  if (title) {
+    articles[i].title = title;
+  }
+  if (description) {
+    articles[i].description = description;
+  }
+  if (author) {
+    articles[i].author = author;
+  }
+  res.status(200);
+  res.json(articles[i]);
+};
+
 app.get("/articles", getAllArticles);
 app.post("/articles", createNewArticle);
 app.get("/articles/search_2", getAnArticleById);
 app.get("/articles/search_1", getArticlesByAuthor);
+app.put("/articles/:id", updateAnArticleById);
 
 app.listen(port, () => {
   console.log(`Server is working on Port : ${port}`);
