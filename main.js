d74558808bd0d72ad9a62d22d9581a1cabb9f6af
TariqBazadough <tariq.bazadough@gmail.com>
2021-05-18 10:03:23 +0300

    done with refactoring get an arcticle by id function

--- main.js
+++ main.js
@@ -9,26 +9,26 @@ const port = 5000;
 
 app.use(express.json());
 
-const articles = [
-  {
-    id: 1,
-    title: "How I learn coding?",
-    description: "Lorem, Quam, mollitia.",
-    author: "Jouza",
-  },
-  {
-    id: 2,
-    title: "Coding Best Practices",
-    description: "Lorem, ipsum dolor sit, Quam, mollitia.",
-    author: "Besslan",
-  },
-  {
-    id: 3,
-    title: "Debugging",
-    description: "Lorem, Quam, mollitia.",
-    author: "Jouza",
-  },
-];
+// const articles = [
+//   {
+//     id: 1,
+//     title: "How I learn coding?",
+//     description: "Lorem, Quam, mollitia.",
+//     author: "Jouza",
+//   },
+//   {
+//     id: 2,
+//     title: "Coding Best Practices",
+//     description: "Lorem, ipsum dolor sit, Quam, mollitia.",
+//     author: "Besslan",
+//   },
+//   {
+//     id: 3,
+//     title: "Debugging",
+//     description: "Lorem, Quam, mollitia.",
+//     author: "Jouza",
+//   },
+// ];
 
 const getAllArticles = (req, res) => {
   Article.find({})
@@ -43,16 +43,14 @@ const getAllArticles = (req, res) => {
 
 const getAnArticleById = (req, res) => {
   const id = req.query.id;
-
-  let i;
-  const found = articles.find((element, index) => {
-    i = index;
-    return element.id == id;
-  });
-  if (found) {
-    res.status(200);
-    res.json(articles[i]);
-  }
+  Article.find({ _id: id })
+    .then((result) => {
+      res.status(200);
+      res.json(result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 };
 
 const getArticlesByAuthor = (req, res) => {
